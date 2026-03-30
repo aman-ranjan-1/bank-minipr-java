80757ffa22aeb30571dca92a14106164a2de639d
aman-ranjan-1 <aman.ranjan21a@gmail.com>
2026-03-30 23:28:43 +0530

    Fixed the clearscreen in BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -6,6 +6,12 @@ public class BankAccount {
     String Username ;  
     private static int AccCount = 0;
     Scanner sc = new Scanner(System.in);
+    // clearScreen method
+    void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+    // account create 
     void CreateAcc(){
         System.out.println("Enter your Name : ");
         Username = sc.nextLine();
@@ -14,21 +20,36 @@ public class BankAccount {
         Random vnum = new Random();
         AccountNum = 100000000 + vnum.nextInt(999999);
         AccountNumber="ACC"+AccountNum;
+        clearScreen();
         System.out.println("Your Account Created ");
         System.out.println("Account Number : "+AccountNumber+"  User - "+Username);
         AccCount++;
     }
+    // acc count
     static void showAccCount(){
         System.out.println("Total Existing Accounts - "+AccCount);
     }
+    // deposit balance
     void DepositBalance(){
         int amount ;
+        clearScreen();
+        System.out.println();
+        System.out.println();
         System.out.println("Enter amount to add to your account : ");
         amount = sc.nextInt();
+        if(amount>0){
         balance = balance + amount;
         System.out.println("Hey "+Username+"! Amount deposited successfully.");
+        }
+        else{
+            System.out.println("Invalid Amount to deposit");
+        }
     }
+    // withdraw balance
     void WithdrawAmount(int pass){
+        clearScreen();
+        System.out.println();
+        System.out.println();
         if(pass == passkey){
             int amount;
             System.out.println("Enter amount to withdraw : ");
@@ -48,8 +69,12 @@ public class BankAccount {
             System.out.println("Wrong Passkey entered.");
         }
     
-    }
+    } 
+    // display balance
     void DisplayBalance(int pass){
+        clearScreen();
+        System.out.println();
+        System.out.println();
         if (pass == passkey) {
             System.out.println("Balance : "+balance);
         }
